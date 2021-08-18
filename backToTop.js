47397276ca4dd9c7e3464ffce5ec8ff32f6893e5
Osman Onat <84340387+OsmanOnat@users.noreply.github.com>
2021-08-18 08:56:57 +0300

    -
    -

--- backToTop.js
+++ backToTop.js
@@ -17,6 +17,7 @@ function scrollTop() {
 window.onscroll = function() {
     if($(this).scrollTop() > 900){ // Eğer scrollTop fonksiyonundaki scrollTop > 1000 ile aynı değeri verirsen , button bug'a girer.
         $(topButton).show("slow");
+        scrollTop();
     }else{
         $(topButton).hide("slow");
     }
